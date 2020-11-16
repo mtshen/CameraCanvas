bfe78551b44a492f1bccf9c26373153f55bda8b5
mtshen <mtshen1997@163.com>
2020-11-16 17:55:38 +0800

    add

---
+++ .history/www/index_20201116171125.js
@@ -0,0 +1,61 @@
+// window.onload = function () {
+//   var canvas = document.getElementsByTagName('canvas')[0],
+//       context = canvas.getContext('2d'),
+//       video = document.getElementsByTagName("video")[0],
+//       snap = document.getElementById("snap"),
+//       close = document.getElementById("close"),
+//       start = document.getElementById("start"),
+//       MediaStreamTrack;
+//   start.addEventListener('click', function () {
+//       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//           navigator.mediaDevices.getUserMedia({
+//               video: true,
+//               audio: true
+//           }).then(function (stream) {
+//               console.log(stream);
+//               MediaStreamTrack=typeof stream.stop==='function'?stream:stream.getTracks()[1];
+//               video.src=(window.URL).createObjectURL(stream);
+//               video.play();
+//           }).catch(function(err){
+//               console.log(err);
+//           });
+//       }else if(navigator.getMedia){
+//           navigator.getMedia({
+//               video: true
+//           }).then(function (stream) {
+//               console.log(stream);
+//               MediaStreamTrack=stream.getTracks()[1];
+//               video.src=(window.webkitURL).createObjectURL(stream);
+//               video.play();
+//           }).catch(function(err){
+//               console.log(err);
+//           });
+//       }
+//   });
+//   snap.addEventListener('click', function () {
+//       context.drawImage(video, 0, 0,200,150);
+//   });
+//   close.addEventListener('click', function () {
+//       MediaStreamTrack && MediaStreamTrack.stop();
+//   });
+// }
+
+var opt = {
+    audio: true,
+    video: {
+     width: 375,
+     height: 603
+    }
+   };
+// 打开摄像头
+navigator.mediaDevices.getUserMedia(opt)
+.then(function(mediaStream) {
+    var video = document.querySelector('video');
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+    video.play();
+    };
+})
+.catch(function(err) {
+    console.log(err.name + ": " + err.message);
+}); // always check for errors at the end.
---
+++ .history/www/index_20201116174713.js
@@ -0,0 +1,90 @@
+const canvasElement = document.querySelector("canvas");
+
+/**
+ * 摄像头动填转存Canvas
+ *
+ * 需要传入
+ * canvas: canvas 元素
+ * width: canvas的宽度, 需要传入具体数值
+ * height: canvas的高度, 需要传入具体数值
+ */
+class CameraCanvas {
+    constructor(option) {
+        const { canvas, width = 600, height = 800 } = option;
+
+        // canvas 是必填数据
+        if (canvas) {
+            console.error("没有得到可用的Canvas数据");
+            return;
+        }
+
+        const videoElement = document.createElement("video");
+
+        // 参数
+        // 视频元素
+        this.videoElement = videoElement;
+        // canvas元素
+        this.canvasElement = canvas;
+        // 2d实例
+        this.canvasElement2d = canvas.getContext("2d");
+        // 宽度
+        this.width = width;
+        // 高度
+        this.height = height;
+        // 是否已经得到了权限
+        this.isAuth = false;
+
+        // 获取权限就系 直接播放到video中
+        videoElement.width = width;
+        videoElement.height = height;
+        videoElement.addEventListener("loadedmetadata", () => {
+            videoElement.play();
+        });
+    }
+
+    // 获取权限
+    getAuth() {
+        const { width, height } = this;
+        // 打开摄像头
+        navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: { width, height }
+        })
+
+        // 当开启摄像头后, 开始载入video开始播放
+        .then(this.getAuthDoneCallBack)
+
+        .catch(this.getAuthErrorCallBack);
+    }
+
+    // 获取权限获取成功的回调
+    getAuthDoneCallBack(mediaStream) {
+        this.isAuth = true;
+        this.videoElement.srcObject = mediaStream;
+    }
+
+    // 获取权限获取失败后的回调
+    getAuthErrorCallBack() {
+        this.isAuth = false;
+        this.videoElement.srcObject = null;
+    }
+
+    // 开始向Canvas渲染
+    startRender() {
+        const { videoElement, canvasElement2d, width, height } = this;
+        if (videoElement.ended) {
+            return;
+        }
+        // 将video上的图片的每一帧以图片的形式绘制的canvas上
+        canvasElement2d.drawImage(videoElement, 0, 0, width, height);
+        window.requestAnimationFrame(this.startRender);
+    }
+
+}
+
+
+new CameraCanvas({
+    canvas: canvasElement,
+    width: canvasElement.offsetWidth,
+    height: canvasElement.offsetHeight,
+});
